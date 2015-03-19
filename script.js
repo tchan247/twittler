336bf3235186c449174b8a5ead92ab432997c020
Terry <tchan247@gmail.com>
2015-03-19 16:16:39 -0700

    Fixed and formatted time stamp. Added month abbrv. and switched to 12-hour clock time (AM/PM)

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ $(document).ready(function(){
   var index = 0;
   var $button = $('.load');
   var remaining;
+  // timestamp variables
   var time = new Date();
+  var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var ampm = function(obj) {
+    var h = obj.getHours();
+    var mer;
+    if(h > 12) {
+      h = h - 12;
+      mer = " PM"
+    } else {
+      mer = " AM"
+    }
+
+    h = h + ":" + obj.getMinutes() + mer;
+
+    return h;
+  }
   
   // init ------------------------------------------------------------------------
   $button.text('Load More Tweets');
@@ -14,16 +30,15 @@ $(document).ready(function(){
   var loadTweets = function(n) {
     while(n > 0){
       var tweet = streams.home[index];
+      var created = tweet.created_at;
       var $tweet = $('<div class=".tweet"></div>');
       var $msg = $('<p></p>').text('@' + tweet.user + ': ' + tweet.message);
-      var $timestamp = $('<p></p>').text(tweet.created_at.toString());
+      var $timestamp = $('<p></p>').text(ampm(created) + " - "+ created.getDay() + " " 
+        + month[created.getMonth()] + " " + created.getYear());
 
       $tweet.append($msg);
       $tweet.append($timestamp);
       $tweet.prependTo($tweets);
-
-      // add timestamp
-      $('')
 
       index ++;
       n--;

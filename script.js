e7523e7cfd2b57fd9b162904e00c9bb47bd66da5
Terry <tchan247@gmail.com>
2015-03-19 14:26:13 -0700

    Fixed html syntax. edited/organized js. Added gitignore: DS_Store

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-        var $body = $('.tweets');
-        var tweets = streams.home.length;
-        var index = 0;
-        
-        // load initial tweets
-        var loadTweets = function() {
-          while(index < tweets){
-            var tweet = streams.home[index];
-            var $tweet = $('<div></div>');
-            $tweet.text('@' + tweet.user + ': ' + tweet.message);
-            $tweet.prependTo($body);
-            index ++;
-          }
-        }
-        loadTweets();
 
-        // check for new tweets
-        setInterval(function(e){
-          tweets = streams.home.length;
-          loadTweets();
-        }, 1000);
+  var $tweets = $('.tweets');
+  var tweets = streams.home.length;
+  var index = 0;
+  
+  // init ------------------------------------------------------------------------
+  // load initial tweets
+  var loadTweets = function() {
+    while(index < tweets){
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.prependTo($tweets);
+      index ++;
+    }
+  }
+  loadTweets();
 
-      });
+  // body ------------------------------------------------------------------------
+  // check for new tweets and hold on to them
+  setInterval(function(e){
+    var remaining = tweets - index;
+    var more = remaining === 0? 'More' : remaining;
+    tweets = streams.home.length;
+    loadTweets();
+
+    // update number on load tweets bar
+    $('.load').text = ('Load ' + more + 'Tweets');
+  }, 1000);
+
+  // load tweets
+  $('.load');
+
+});
